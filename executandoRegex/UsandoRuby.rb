0b8c02a9b026792d481d697e4b1a6f4ed01d37b1
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-05-14 21:50:58 -0300

    Finalizada Seção 2

---
+++ executandoRegex/UsandoRuby.rb
@@ -0,0 +1,19 @@
+texto = '0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f'
+
+regexNove = Regexp::new('9')
+puts regexNove.match(texto)
+
+regexXis = Regexp::new('x')
+puts regexXis.match(texto)
+
+regexNove = %r{9}
+puts regexNove.match(texto)
+
+puts regexNove =~ '894'
+
+regexLetras = /[a-f]/
+puts texto.scan(regexLetras).join(' ')
+
+puts texto.split(/,/).join(' ')
+
+print texto.split(/[aeiou]/)
